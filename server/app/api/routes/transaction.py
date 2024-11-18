fe7ce5456bcf47801d8be9c106439ea54bc49336
MT XA <21020382@vnu.edu.vn>
2024-11-18 18:46:31 +0700

    fix: call api transaction

--- server/app/api/routes/transaction.py
+++ server/app/api/routes/transaction.py
@@ -1,23 +1,24 @@
 from fastapi import APIRouter
 from api.deps import SessionDep
 import api.applications.transaction.transaction_controller as TransactionController
-from app.models.transactions import Transaction, TransactionListOut, TransactionOut
+from app.models.transactions import Transaction, TransactionIn, TransactionListOut, TransactionOut
+from app.models.budgets import Budget
 
 router = APIRouter()
 
-@router.get('/{transaction_id}')
+@router.get('/get/{transaction_id}')
 def get_transaction(session: SessionDep, transaction_id: int):
   return TransactionController.read_transaction(session, transaction_id)
 
 @router.post('/')
-def create_transaction(session: SessionDep, transaction: Transaction):
-  return TransactionController.create_transaction(session, transaction)
+def create_transaction(session: SessionDep, data: TransactionIn):
+  return TransactionController.create_transaction(session, data)
 
-@router.delete('/{transaction_id}')
+@router.delete('/delete/{transaction_id}')
 def delete_transaction(session: SessionDep, transaction_id: int):
   return TransactionController.delete_transaction(session, transaction_id)
 
-@router.put('/{transaction_id}')
+@router.put('/update/{transaction_id}')
 def update_transaction(session: SessionDep, transaction_id: int, transaction: Transaction):
   return TransactionController.update_transaction(session, transaction_id, transaction)
 
--- server/app/models/transactions.py
+++ server/app/models/transactions.py
@@ -10,12 +10,12 @@ class TransactionBase(SQLModel):
 
 class Transaction(TransactionBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    user_id: int = Field(default=None, foreign_key="user.id")
+    # user_id: int = Field(default=None, foreign_key="user.id")
     description: str 
     amount: int | None 
     date: datetime = Field(default=datetime.now())
     budget_id: int = Field(default=None, foreign_key="budget.id")
-    user: "User" = Relationship(back_populates="transactions")
+    # user: "User" = Relationship(back_populates="transactions")
     budget: "Budget" = Relationship(back_populates="transactions")
 
     class Config:
@@ -26,14 +26,15 @@ class TransactionOut(TransactionBase):
     description: str
     amount: int 
     budget: BudgetOut
-    user: UserOut
+    # user: UserOut
 
 class TransactionIn(TransactionBase): 
-    user_id: int
-    budget_id: int
+    # user_id: int
+    budget: str
     description: str
     amount: int
 
 class TransactionListOut(SQLModel): 
     data: list[Transaction]
     count: int
+
