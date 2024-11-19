c3b8f3ee4ce6e39fa8ed24871089bb7edaecc2a1
MT XA <21020382@vnu.edu.vn>
2024-11-19 07:45:53 +0700

    update: update transaction api

--- server/app/models/transactions.py
+++ server/app/models/transactions.py
@@ -12,9 +12,10 @@ class Transaction(TransactionBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     # user_id: int = Field(default=None, foreign_key="user.id")
     description: str 
+    budgetName: str | None
     amount: int | None 
     date: datetime = Field(default=datetime.now())
-    budget_id: int = Field(default=None, foreign_key="budget.id")
+    budgetID: int = Field(default=None, foreign_key="budget.id")
     # user: "User" = Relationship(back_populates="transactions")
     budget: "Budget" = Relationship(back_populates="transactions")
 
