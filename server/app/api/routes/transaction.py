3b242ece04515a72b1fb3ec462509b6544250672
Bùi Minh Quân <100108186+21020382-Quan@users.noreply.github.com>
2024-11-25 19:56:07 +0700

    feat: add api get by user

--- server/app/api/routes/transaction.py
+++ server/app/api/routes/transaction.py
@@ -7,8 +7,8 @@ from app.models.budgets import Budget
 router = APIRouter()
 
 @router.get('/get/{transaction_id}')
-def get_transaction(session: SessionDep, transaction_id: int):
-  return TransactionController.read_transaction(session, transaction_id)
+def get_transaction(session: SessionDep, user_id: int, transaction_id: int):
+  return TransactionController.read_transaction(session, user_id, transaction_id)
 
 @router.post('/')
 def create_transaction(session: SessionDep, data: TransactionIn):
@@ -22,6 +22,10 @@ def delete_transaction(session: SessionDep, transaction_id: int):
 def update_transaction(session: SessionDep, transaction_id: int, transaction: Transaction):
   return TransactionController.update_transaction(session, transaction_id, transaction)
 
-@router.get('/get_all_transactions')
+@router.get('/get_all_transactions/{user_id}')
 def get_all_transactions(session: SessionDep, user_id: int) -> TransactionListOut: 
   return TransactionController.read_all_transactions(session, user_id)
+
+@router.get('/get_all_transactions_by_budget/{budget_id}')
+def get_all_transactions_by_budget(session: SessionDep, budget_id: int) -> TransactionListOut: 
+  return TransactionController.read_all_transactions_by_budget(session, budget_id)
